2f5445f2456b1df9cb9687d5acd84f98e9f1e8bc
Andrew J. Leer <leeand00@hotmail.com>
2017-07-17 09:01:31 -0400

    Added missing library required by postfix.

--- modules/postfix/manifests/install.pp
+++ modules/postfix/manifests/install.pp
@@ -2,10 +2,14 @@ class postfix::install {
 	package {'postfix':
 		ensure => installed,
  	}
+
+	package {'libsasl2-modules':
+		ensure => installed,
+	}
 	
 	service {'postfix':
 	 	ensure => running,
 		enable => true,
-		require => Package['postfix']	
+		require => [Package['postfix'], Package['libsasl2-modules']]
 	}
 }
