e043aecae8dbf8ffe4c9e7d1dc628754fa3b8a6e
Andrew J. Leer <leeand00@hotmail.com>
2017-07-21 14:54:14 -0400

    Fixed issue with FileDaemon assinged to wrong ethX for the FileDaemon
    Changed the address of the Directors File Daemon to the original
    ethernet card (eth0)

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -443,7 +443,7 @@ node 'bareOSdirector' {
      # Client (File Services) to backup
      bareos::director::client {"${hostname}-fd":
 	name => "${hostname}-fd",
-        address => $ipaddress_eth2,
+        address => $ipaddress_eth0,
 	catalog => 'MyCatalog',  # See `Creates a catalog...`
 	file_retention => '6 months',  # Should be 6 months or so until you learn bacula better. 
 	job_retention => '1 year',     # Should be equal to your maximum volume_retention (see the Monthly pool)
