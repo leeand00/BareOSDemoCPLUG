08523e85f9aec48887dd925a23f6681ee25a0e66
Andrew J. Leer <leeand00@hotmail.com>
2017-07-21 14:51:54 -0400

    Added Off-site Pools for bareOSremoteSD
    Added Off-site(Monthly|Weekly|Daily) backups for backing up to the
    bareOSremoteSD (BareOS Remote Storage Daemon)

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -224,6 +224,57 @@ node 'bareOSdirector' {
 	storage => 'bareOSdirector_FileStorage',
      }
 
+# Off-site Backup Pools
+     
+    bareos::director::pool{'Offsite-Daily':
+        type => 'Backup',
+        recycle => 'yes',
+        auto_prune => 'yes',
+        volume_use_duration => '4 days',
+        volume_retention => '9 days',
+	maximum_volume_bytes => '10G',       # NOTE: A 1G setting on this overflowed into another vol, so I'm trying 10G. 
+        maximum_volume_jobs => '10',
+        maximum_volumes => '10', # Should be multiplied by Maximum Volume Bytes to make sure you don't
+                                 # over fill the disk or KVM Logical Volume.
+     	label_format => 'offsite-daily-${NumVols}',  # Note: There's no point in using the date variable here, 
+                                             #       since it's stored elsewhere in the volume meta data.
+					     #       Should craete files named "offsite-daily-1", "offsite-daily-2", etc..
+     	storage => 'bareOSremoteSD' # Set to use offsite storage...
+     }
+
+     bareos::director::pool{'Offsite-Weekly':
+        type => 'Backup',
+        recycle => 'yes',
+        auto_prune => 'yes',
+        volume_use_duration => '70 hours',   # 70 hours ~= 3 days
+        volume_retention => '28 days',       # Could use a little less or a little more about every 
+					     # 4 weeks these are recycled.
+	maximum_volume_bytes => '10G',
+	maximum_volume_jobs => '100',
+        maximum_volumes => '10', # Should be multiplied by Maximum Volume Bytes to make sure you don't
+                                 # over fill the disk or KVM Logical Volume.
+     	label_format => 'offsite-weekly-${NumVols}',  # Note: There's no point in using the date variable here, 
+                                             #       since it's stored elsewhere in the volume meta data.
+					     #       Should craete files named "offsite-weekly-1", "offsite-weekly-2", etc..
+     	storage => 'bareOSremoteSD' # Set to use offsite storage...
+     }
+
+     bareos::director::pool{'Offsite-Monthly':
+        type => 'Backup',
+        recycle => 'yes',		     # Bacula can automatically recycle volumes. 
+        auto_prune => 'yes',                 # Prune expired volumes
+        volume_use_duration => '70 hours',   # 70 hours ~= 3 days
+        volume_retention => '362 days',      # One year
+	maximum_volume_bytes => '10G',
+        maximum_volume_jobs => '100', 
+	maximum_volumes => '10', # Should be multiplied by Maximum Volume Bytes to make sure you don't
+                                 # over fill the disk or KVM Logical Volume.
+     	label_format => 'offsite-monthly-${NumVols}',  # Note: There's no point in using the date variable here, 
+                                               #       since it's stored elsewhere in the volume meta data.
+				    	       #       Should craete files named "offsite-monthly-1", "offsite-monthly-2", etc..
+     	storage => 'bareOSremoteSD' # Set to use offsite storage...
+     }
+
 # Storage
      # File Storage for ${hostname}-fd backup...
      # TODO: Rename this...
