6f3bde0abc1e729ef6afb2ef9e6c648c38e47512
Andrew J. Leer <leeand00@hotmail.com>
2017-07-21 15:26:36 -0400

    Added Schedules for Example Offsite Pools
    Theses can be verified as working by checking in the bconsole by
    running:
    
    *status schedule
    
    and they'll show up in the "Scheduler Preview for n days:"

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -431,10 +431,10 @@ node 'bareOSdirector' {
 #                    ['Incremental', 'mon-fri', '23:05']],
 
        run_spec => [
-                    ['Differential', 'Pool=Daily monday-thursday', '23:30'], # 11pm (sons)
-                    ['Full', 'Pool=Weekly 2nd-5th Friday', '23:30'], # 11pm (fathers)  
+                    ['Differential', 'Pool=Offsite-Daily monday-thursday', '23:30'], # 11pm (sons)
+                    ['Full', 'Pool=Offsite-Weekly Friday', '23:30'], # 11pm (fathers)  
 								     # Note: 2nd - 5th Friday because some months have 5 fridays in them.
-                    ['Full', 'Pool=Monthly 1st Friday', '23:30'] # First Friday of the month Montly backup is done.
+                    ['Full', 'Pool=Offsite-Monthly 1st Friday', '23:30'] # First Friday of the month Montly backup is done.
 								 # 11pm (grandfathers)
 
                    ], 
